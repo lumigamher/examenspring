5cea0794d402b9e059419cd1a3b5b593b056bce6
lumigamher <lumigam@hotmail.com>
2024-08-13 06:58:29 -0500

    feat: :tada: First Commit!

---
+++ src/main/java/com/campus/examenspring/ExamenspringApplication.java
@@ -0,0 +1,13 @@
+package com.campus.examenspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenspringApplication.class, args);
+	}
+
+}
